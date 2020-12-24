2cbe27a349b2d1888f0d0f389859953658a51c63
aixiaolong <342826844@qq.com>
2020-12-24 14:31:12 +0800

    fix: 配置优化

--- src/api/request.ts
+++ src/api/request.ts
@@ -8,7 +8,7 @@ axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8';
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL: '/api',
+  baseURL: process.env.VUE_APP_BASE_API,
   // 超时
   timeout: 10000,
 });
@@ -22,7 +22,6 @@ service.interceptors.request.use((config) => {
   }
   return config;
 }, (error) => {
-  console.log(error);
   Promise.reject(error);
 });
 
